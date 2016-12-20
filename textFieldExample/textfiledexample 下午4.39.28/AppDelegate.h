9416e47e8f609eb7ee8af437262506a8f663df5c
dalengdaleng <city_date41@163.com>
2016-12-20 16:40:31 +0800

    commit

---
+++ textFieldExample/textfiledexample 下午4.39.28/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textfiledexample
+//
+//  Created by ios on 16/10/21.
+//  Copyright © 2016年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textFieldExample/textfiledexample 下午4.39.28/TextFieldTint.h
@@ -0,0 +1,14 @@
+//
+//  TextFieldTint.h
+//  textfiledexample
+//
+//  Created by ios on 16/10/21.
+//  Copyright © 2016年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextFieldTint : UITextField
+-(void) setColorButtonClearHighlighted:(UIColor *)colorButtonClearHighlighted;
+-(void) setColorButtonClearNormal:(UIColor *)colorButtonClearNormal;
+@end
